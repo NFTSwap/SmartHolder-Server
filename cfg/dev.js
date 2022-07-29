cc7ad788f65160c22cbdda0011dcd1115681b832
chuxuewen <louistru@hotmail.com>
2022-07-29 11:05:29 +0800

    update cfg

--- cfg/dev.js
+++ cfg/dev.js
@@ -9,10 +9,17 @@ module.exports = {
 		RINKEBY: rpc.RINKEBY,
 	},
 	contractImpls: impl,
-	mbus: 'mqtt://192.168.0.189:1883',
-	mbus_auth: 'nft_mqtt_dev:inmyshowD3', // user:password
+	mvp_ser_api: 'https://mvp.stars-mine.com/service-api',
+	// ssh -f -N -g -R 8002:0.0.0.0:8002 root@dttyd.stars-mine.com
+	publicURL: 'https://smart-dao-dev.stars-mine.com',
+	mbus: 'mqtt://mqtt-test.stars-mine.com:2883',
+	mbus_auth: 'nft_hardware_test:nft_hardware_test', // user:password
+	mbus_topic: 'shs_default_dev',
+	enable_auth: true,
 	env: 'dev',
 	mysql: {
-		host: '192.168.0.189', port: 3306, user: 'root', password: 'uzBGtlhlHnAQFtxdKj*Z', database: 'mvp', // dev inner
+		host: '192.168.0.189', port: 3306, user: 'smartholder', password: 'C1fNdYiPk0Tw5A==smartholder', database: 'smartholder', // dev inner
+		//host: 'ddata.stars-mine.com', port: 22022, user: 'chuxuewen', password: '%5KNRKDQXcvBq358DkWQxtunVN5Uu61', database: 'smartholder', // dev inner
 	},
+	fastStart: false,
 };
