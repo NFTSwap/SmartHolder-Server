928295a588e4fbe19619a11167af29de8d62abf9
chuxuewen <louistru@hotmail.com>
2022-08-19 15:38:34 +0800

    update asset meta

--- src/sync/asset_meta.ts
+++ src/sync/asset_meta.ts
@@ -300,7 +300,7 @@ export class AssetMetaDataUpdate implements WatchCat {
 	async cat() {
 
 		for (let k of Object.keys(web3s)) {
-			let chain = (ChainType as any)[k] as ChainType;
+			let chain = Number(k) as unknown as ChainType;
 			let day = 10*24*3600*1e3; // 10天扫描一次数据
 			let offset = await storage.get(`NFTAssetDataSync_Offset_${chain}`, 0) as number;
 			if (offset === 0) {
