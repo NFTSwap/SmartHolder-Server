89c5317787fdbb13c207590d9eb2fa225146d777
chuxuewen <louistru@hotmail.com>
2023-04-18 11:09:52 +0800

    update deps and cfg

--- cfg/prod.js
+++ cfg/prod.js
@@ -20,5 +20,6 @@ module.exports = {
 		host: '172.16.2.46', port: 3306, user: 'smartdao', password: 'smartdao', database: 'smartdao',
 	},
 	redis: 'redis://172.16.3.114:6379/10', // redis cfg
+	redis_pwd: 'HqNuWY8WRSG2G*&^%#NGRDfmj',
 	atomicLock: 'http://172.16.3.114:9802', // atomic lock service
 };
