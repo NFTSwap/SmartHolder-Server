4ee07a441847f06a4938998692f4db4b0f5cfbeb
chuxuewen <louistru@hotmail.com>
2023-05-30 11:34:16 +0800

    update cfg rpc

--- cfg/util/rpc.js
+++ cfg/util/rpc.js
@@ -1,13 +1,7 @@
 
 module.exports = {
 	ETHEREUM: [
-		// '8/http:172.16.1.93:8545', // jp geth
-		// '8/http:172.16.1.119:8545', // jp geth
-		'https://eth-mainnet.g.alchemy.com/v2/jwLMO_t2Vd8LXMW5kBImmFtHNiwDf8kM',
-		'https://eth-mainnet.g.alchemy.com/v2/GowKUhfgb_dUokKbJd25fCQmu_Rh4OOQ',
-		// -----------------------------------------------------------
-		// 'https://mainnet.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
-		// -----------------------------------------------------------
+		// -----------
 		// 'https://mainnet.infura.io/v3/eb482df3997d45599d7b1798be60fec9', // third-party@hard-chain.cn
 		// 'https://mainnet.infura.io/v3/52963da6ae3a4e68a7506f982c196701', // zhuce@stars-mine.com
 		// 'https://mainnet.infura.io/v3/84456ff83aca4daa9e8d5da84d7dd6fb', // chuxuewen@hard-chain.cn
@@ -21,52 +15,55 @@ module.exports = {
 		// 'https://mainnet.infura.io/v3/81e184a58fd04f8ea6c7d026f26c34d5', // yuyongpeng@hotmail.com
 		// 'https://mainnet.infura.io/v3/ccab5917a2b84bb4a64d1bc8e5c07fd1', // moqi.reg@gmail.com
 		// 'https://mainnet.infura.io/v3/12c4da3c5080480db4ae34fec7cf7d2c', // yuyongpeng@hard-chain.cn
-		// -----------------------------------------------------------
-		// -----------------------------------------------------------
+		// -----------
+		'https://mainnet.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
 		// 'https://mainnet.infura.io/v3/6b4f3897597e41d1adc12b7447c84767', // louis.tru@gmail.com
+		// 'https://eth-mainnet.g.alchemy.com/v2/jwLMO_t2Vd8LXMW5kBImmFtHNiwDf8kM',
+		// 'https://eth-mainnet.g.alchemy.com/v2/GowKUhfgb_dUokKbJd25fCQmu_Rh4OOQ',
 	],
 	GOERLI: [
-		// 'https://eth-goerli.g.alchemy.com/v2/lwDFslNRvhCjzogUVLPtPzGIN4ZZDa8I',
-		// -----------------------------------------------------------
-		// 'https://goerli.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
-		// -----------------------------------------------------------
-		'https://goerli.infura.io/v3/eb482df3997d45599d7b1798be60fec9', // third-party@hard-chain.cn
-		'https://goerli.infura.io/v3/52963da6ae3a4e68a7506f982c196701', // zhuce@stars-mine.com
-		'https://goerli.infura.io/v3/84456ff83aca4daa9e8d5da84d7dd6fb', // chuxuewen@hard-chain.cn
-		'https://goerli.infura.io/v3/295cc6a3082a464cbf5c77562c0d78d7', // louistru@hotmail.com
-		'https://goerli.infura.io/v3/5fad8ac4c3c946f489e0a5fec628d40e', // louistru@live.com
-		'https://goerli.infura.io/v3/1ad04dd9dfa3421ca28519a575c959d9', // jfm.s@163.com
-		'https://goerli.infura.io/v3/dde6a07efab74ec2998d48ff4a77ec03', // 97071932@qq.com
-		'https://goerli.infura.io/v3/09e23dcd17934ab0b88c9e091789193e', // 2357779577@qq.com
-		'https://goerli.infura.io/v3/faf06a5d35454aeaad87969b0aba90a8', // louistru@tom.com
-		'https://goerli.infura.io/v3/96af19f43a3d4e7cac531d93c431f5cf', // louistru2@tom.com
-		'https://goerli.infura.io/v3/81e184a58fd04f8ea6c7d026f26c34d5', // yuyongpeng@hotmail.com
-		'https://goerli.infura.io/v3/ccab5917a2b84bb4a64d1bc8e5c07fd1', // moqi.reg@gmail.com
-		'https://goerli.infura.io/v3/12c4da3c5080480db4ae34fec7cf7d2c', // yuyongpeng@hard-chain.cn
-		// -----------------------------------------------------------
-		// -----------------------------------------------------------
+		// -----------
+		// 'https://goerli.infura.io/v3/eb482df3997d45599d7b1798be60fec9', // third-party@hard-chain.cn
+		// 'https://goerli.infura.io/v3/52963da6ae3a4e68a7506f982c196701', // zhuce@stars-mine.com
+		// 'https://goerli.infura.io/v3/84456ff83aca4daa9e8d5da84d7dd6fb', // chuxuewen@hard-chain.cn
+		// 'https://goerli.infura.io/v3/295cc6a3082a464cbf5c77562c0d78d7', // louistru@hotmail.com
+		// 'https://goerli.infura.io/v3/5fad8ac4c3c946f489e0a5fec628d40e', // louistru@live.com
+		// 'https://goerli.infura.io/v3/1ad04dd9dfa3421ca28519a575c959d9', // jfm.s@163.com
+		// 'https://goerli.infura.io/v3/dde6a07efab74ec2998d48ff4a77ec03', // 97071932@qq.com
+		// 'https://goerli.infura.io/v3/09e23dcd17934ab0b88c9e091789193e', // 2357779577@qq.com
+		// 'https://goerli.infura.io/v3/faf06a5d35454aeaad87969b0aba90a8', // louistru@tom.com
+		// 'https://goerli.infura.io/v3/96af19f43a3d4e7cac531d93c431f5cf', // louistru2@tom.com
+		// 'https://goerli.infura.io/v3/81e184a58fd04f8ea6c7d026f26c34d5', // yuyongpeng@hotmail.com
+		// 'https://goerli.infura.io/v3/ccab5917a2b84bb4a64d1bc8e5c07fd1', // moqi.reg@gmail.com
+		// 'https://goerli.infura.io/v3/12c4da3c5080480db4ae34fec7cf7d2c', // yuyongpeng@hard-chain.cn
+		// -----------
+		'https://goerli.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
 		// 'https://goerli.infura.io/v3/6b4f3897597e41d1adc12b7447c84767', // louis.tru@gmail.com
+		// 'https://eth-goerli.g.alchemy.com/v2/lwDFslNRvhCjzogUVLPtPzGIN4ZZDa8I',
 	],
 	ARBITRUM_GOERLI: [
 		'https://arbitrum-goerli.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
-		'https://arbitrum-goerli.infura.io/v3/6b4f3897597e41d1adc12b7447c84767' // louis.tru@gmail.com
+		// 'https://arbitrum-goerli.infura.io/v3/6b4f3897597e41d1adc12b7447c84767' // louis.tru@gmail.com
 	],
 	ARBITRUM: [
 		'https://arbitrum-mainnet.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
-		'https://arbitrum-mainnet.infura.io/v3/6b4f3897597e41d1adc12b7447c84767' // louis.tru@gmail.com
+		// 'https://arbitrum-mainnet.infura.io/v3/6b4f3897597e41d1adc12b7447c84767' // louis.tru@gmail.com
 	],
 	MATIC: [
-		'https://rpc-mainnet.maticvigil.com/v1/2db5bb18b8b1b3f1b47a080e4c9ce6d2c7a5128c', // louistru@live.com
-		'https://rpc-mainnet.maticvigil.com/v1/a20a1a70e42a8e7a1e20dbec5bce46adcd36cdab', // louis.tru@gmail.com
-		'https://rpc-mainnet.maticvigil.com/v1/dfd0568eb46b0f97a31bbddd83ccc9dd80c6ef08', // chuxuewen@hard-chain.cn
-		'https://rpc-mainnet.maticvigil.com/v1/f9aa416097c89301b2a74b5ee9a48a3d49987463', // yuyongpeng@hotmail.com
-		'https://rpc-mainnet.maticvigil.com/v1/4ea0aeeeb8f8b2d8899acfc89e9852a361bf5b13', // louistru@hotmail.com
-		'https://rpc-mainnet.maticvigil.com/v1/e5092d150f5bf03f3db004c01499a0786dc0f580', // yuyongpeng@hard-chain.cn
-		'https://rpc-mainnet.maticvigil.com/v1/930f643c173725dadaf2d1b881c1b9743b8bbf62', // third-party@hard-chain.cn
-		'https://rpc-mainnet.maticvigil.com/v1/ef8f16191b474bb494f33283a81a38487e4dc245', // louistru@tom.com
-		'https://rpc-mainnet.maticvigil.com/v1/b706532e6bd03d1b5c93ba66d91a50aebf212dde', // louistru2@tom.com
-		'https://rpc-mainnet.maticvigil.com/v1/368d0a19a1108e223d0dca757a342716f403dbb6', // moqi.reg@gmail.com
-		'optimize:https://polygon-mainnet.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // dedicated to indexer
+		// -----------
+		// 'https://rpc-mainnet.maticvigil.com/v1/2db5bb18b8b1b3f1b47a080e4c9ce6d2c7a5128c', // louistru@live.com
+		// 'https://rpc-mainnet.maticvigil.com/v1/a20a1a70e42a8e7a1e20dbec5bce46adcd36cdab', // louis.tru@gmail.com
+		// 'https://rpc-mainnet.maticvigil.com/v1/dfd0568eb46b0f97a31bbddd83ccc9dd80c6ef08', // chuxuewen@hard-chain.cn
+		// 'https://rpc-mainnet.maticvigil.com/v1/f9aa416097c89301b2a74b5ee9a48a3d49987463', // yuyongpeng@hotmail.com
+		// 'https://rpc-mainnet.maticvigil.com/v1/4ea0aeeeb8f8b2d8899acfc89e9852a361bf5b13', // louistru@hotmail.com
+		// 'https://rpc-mainnet.maticvigil.com/v1/e5092d150f5bf03f3db004c01499a0786dc0f580', // yuyongpeng@hard-chain.cn
+		// 'https://rpc-mainnet.maticvigil.com/v1/930f643c173725dadaf2d1b881c1b9743b8bbf62', // third-party@hard-chain.cn
+		// 'https://rpc-mainnet.maticvigil.com/v1/ef8f16191b474bb494f33283a81a38487e4dc245', // louistru@tom.com
+		// 'https://rpc-mainnet.maticvigil.com/v1/b706532e6bd03d1b5c93ba66d91a50aebf212dde', // louistru2@tom.com
+		// 'https://rpc-mainnet.maticvigil.com/v1/368d0a19a1108e223d0dca757a342716f403dbb6', // moqi.reg@gmail.com
+		// -----------
+		'optimize:https://polygon-mainnet.infura.io/v3/b39e21d67b324543a5718f9a8ebf1dd2', // reloft0716@gmail.com
+		// 'optimize:https://polygon-mainnet.infura.io/v3/6b4f3897597e41d1adc12b7447c84767', // louis.tru@gmail.com
 	],
 	MUMBAI: [
 		'https://rpc-mumbai.maticvigil.com/v1/2db5bb18b8b1b3f1b47a080e4c9ce6d2c7a5128c', // louistru@live.com
