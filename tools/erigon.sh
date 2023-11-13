802c31b7dc08a4a8e7832463a21978d4acdf72f6
chuxuewen <louistru@hotmail.com>
2023-11-13 16:13:06 +0800

    update tool

--- tools/erigon.sh
+++ tools/erigon.sh
@@ -8,7 +8,7 @@
 --datadir=/data/erigon/goerli --chain=goerli --private.api.addr=127.0.0.1:9190 --http.port=8645 --torrent.port=42169  --authrpc.port=8651
 # polygon bor
 /home/blue/Install/erigon_2.53.4_linux_amd64/erigon \
---datadir=/data/erigon/bor --chain=bor-mainnet --private.api.addr=127.0.0.1:9290 --http.port=8745 --torrent.port=42269  --authrpc.port=8751
+--datadir=/data/erigon/bor --chain=bor-mainnet --private.api.addr=127.0.0.1:9290 --http.port=8745 --torrent.port=42269 --snapshots=true --authrpc.port=8751
 
 
 # download ploygon
