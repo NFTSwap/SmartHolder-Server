f9e7c866d57ef4787ef5437771eb2bf1006ff5de
chuxuewen <louistru@hotmail.com>
2023-11-15 16:35:06 +0800

    update cfg

--- tools/erigon.sh
+++ tools/erigon.sh
@@ -4,8 +4,11 @@
 # mainnet
 /home/blue/Install/erigon_2.53.4_linux_amd64/erigon --datadir=/data/erigon/mainnet --chain=mainnet --verbosity=5
 # goerli
+/home/blue/Install/erigon_2.53.4_linux_amd64/erigon --datadir=/data/erigon/goerli --chain=goerli --verbosity=5
 /home/blue/Install/erigon_2.53.4_linux_amd64/erigon \
 --datadir=/data/erigon/goerli --chain=goerli --private.api.addr=127.0.0.1:9190 --http.port=8645 --torrent.port=42169  --authrpc.port=8651 --verbosity=5
+# ./build/bin/erigon --datadir="<your_mainnet_data_path>" --chain=mainnet --port=30303 --http.port=8545 --torrent.port=42069 --private.api.addr=127.0.0.1:9090 \
+# --http --ws --http.api=eth,debug,net,trace,web3,erigon
 # polygon bor
 /home/blue/Install/erigon_2.53.4_linux_amd64/erigon \
 --datadir=/data/erigon/bor --chain=bor-mainnet --private.api.addr=127.0.0.1:9290 --http.port=8745 --torrent.port=42269 --snapshots=true --authrpc.port=8751
