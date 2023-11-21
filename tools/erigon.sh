520b3a0c6b92da5b0c2aa465f79ab0b7536ef62f
chuxuewen <louistru@hotmail.com>
2023-11-21 14:38:29 +0800

    update tools

--- tools/erigon.sh
+++ tools/erigon.sh
@@ -1,5 +1,5 @@
 #!/bin/env node
-# erigon start config
+# Erigon
 
 # mainnet
 /home/blue/Install/erigon_2.53.4_linux_amd64/erigon --datadir=/data/erigon/mainnet --chain=mainnet --verbosity=5 --internalcl 
@@ -22,27 +22,24 @@
 --torrent.port=43068 --private.api.addr=127.0.0.1:9290 --http.compression --http.vhosts=* --http.corsdomain=* \
 --http.api=eth,debug,net,trace,web3,erigon,bor --ws --ws.compression --rpc.gascap=300000000 --verbosity=5 --p2p.allowed-ports="30307,30308,30309,30310"
 
+# Polygon
+
 # polygon heimdall
 # /var/lib/heimdall/config/config.toml
 # seeds="1500161dd491b67fb1ac81868952be49e2509c9f@52.78.36.216:26656,dd4a3f1750af5765266231b9d8ac764599921736@3.36.224.80:26656,8ea4f592ad6cc38d7532aff418d1fb97052463af@34.240.245.39:26656,e772e1fb8c3492a9570a377a5eafdb1dc53cd778@54.194.245.5:26656,6726b826df45ac8e9afb4bdb2469c7771bd797f1@52.209.21.164:26656"
-# seeds="f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656,2eadba4be3ce47ac8db0a3538cb923b57b41c927@35.199.4.13:26656,3b23b20017a6f348d329c102ddc0088f0a10a444@35.221.13.28:26656,25f5f65a09c56e9f1d2d90618aa70cd358aa68da@35.230.116.151:26656"
-# seeds="2a53a15ffc70ad41b6876ecbe05c50a66af01e20@3.211.248.31:26656,6f829065789e5b156cbbf076f9d133b4d7725847@3.212.183.151:26656,7285a532bad665f051c0aadc31054e2e61ca2b3d@3.93.224.197:26656,0b431127d21c8970f1c353ab212be4f1ba86c3bf@184.73.124.158:26656,f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,31b79cf4a628a4619e8e9ae95b72e4354c5a5d90@44.232.55.71:26656,a385dd467d11c4cdb0be8b51d7bfb0990f49abc3@35.199.4.13:26656,daad548c0a163faae1d8d58425f97207acf923fd@35.230.116.151:26656,81c76e82fcc3dc9a0a1554a3edaa09a632795ea8@35.221.13.28:26656"
 heimdalld init --home=/data/polygon/heimdall --chain=mainnet
 heimdalld start --home=/data/polygon/heimdall --chain=mainnet --rest-server
+# polygon bor
+# bootnodes = ["enode://bdcd4786a616a853b8a041f53496d853c68d99d54ff305615cd91c03cd56895e0a7f6e9f35dbf89131044e2114a9a782b792b5661e3aff07faf125a98606a071@43.200.206.40:30303", "enode://209aaf7ed549cf4a5700fd833da25413f80a1248bd3aa7fe2a87203e3f7b236dd729579e5c8df61c97bf508281bae4969d6de76a7393bcbd04a0af70270333b3@54.216.248.9:30303"]|g' /var/lib/bor/config.toml
+# port=8845 p2p=30323
+bor server -config /data/polygon/bor/config.toml
 
 # install heimdall
-curl -L https://raw.githubusercontent.com/maticnetwork/install/main/heimdall.sh | bash -s -- v0.3.5 mainnet sentry
-# sudo dpkg -r heimdalld
-# echo "Installing $package ..."
-# sudo dpkg -i $package
-# if [ ! -z "$profilePackage" ] && sudo [ ! -d /var/lib/heimdall/config ]; then
-# 		sudo dpkg -i $profilePackage
-# fi
+curl -L https://raw.githubusercontent.com/maticnetwork/install/main/heimdall.sh | bash -s -- v1.0.3 mainnet sentry
+# install bor
+curl -L https://raw.githubusercontent.com/maticnetwork/install/main/bor.sh | bash -s -- v1.1.0 mainnet sentry
 
-# download polygon
+# download heimdall snapshow
 nohup curl -L https://snapshot-download.polygon.technology/snapdown.sh | bash -s -- --network mainnet --client heimdall --extract-dir heimdall-snapshow --validate-checksum true > /dev/null 2>&1 &
+# download bor erigon snapshow
 nohup curl -L https://snapshot-download.polygon.technology/snapdown.sh | bash -s -- --network mainnet --client erigon --extract-dir erigon-snapshow --validate-checksum true >/dev/null 2>&1 &
-
-# 332537972	goerli
-# 148061156	mainnet
-# 408536240	mainnet.bk
